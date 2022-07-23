39369ad870536ed165dad5b7af0fe50efadb8e02
ISODEC2022 <109834534+ISODEC2022@users.noreply.github.com>
2022-07-23 10:13:29 -0500

    Add files via upload

---
+++ pytodos.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 22 19:31:48 2022
+
+@author: Franco Fabián
+"""
+
+x=1
+print(x)
+x=x+1
+print(x)
+
+x=0.5
+x=2.5*x*(2-x)
+
+
+23%5
+
+x = 2 + 2**4 / 2 *5
+print(x)
+
+# Integers
+x = 1
+y = 2
+z = 3. # no entero
+
+print(type(x), type(y), type(z))
+
+z = int(3.2)
+print(z, type(z))
+
+# Floats
+x = 1.2
+y = 22*3.33
+z = 1.4
+
+print(type(x), type(y), type(z))
+
+entero = int(5)
+print(x+entero)
+print(y-entero)
+print(z*entero)
+print(z/entero)
+print(entero/z)
+
+# String
+x = 'Python "básico"'
+y = 'primera'
+z = 'clase'
+
+print(x + y + z)
+print(x, y, z)
+
+# Boolean
+print(type(True))
+print(False)
+
+# Arrays
+nombres = ['Carlos', 'Nora', 'Pedro']
+print(nombres[0])
+print(len(nombres))
+print(type(nombres))
+
+# List
+milista = [1, 3.2, 'Text', True]
+
+print(milista[0])
+print(len(milista))
+print(type(milista))
+
+nombre=input('¿Quién eres?')
+print('Hola',nombre)
+
+x='hola'
+type(x)
+
+a=10
+b=float(a)
+print(b)
+type(b)
+
+float(a)
+type(a)
+
+
+entrada = input('Número de orden ')
+ordpy = int(entrada)-1
+print('El orden en Python es:',ordpy)
+
+
+x = 2
+x=x+10
+print(x)
+
+n =3+6**2/3*2
+print(n)
+type(n)
+
+entrada = input('Número de orden ')
+ordpy = int(entrada)-1
+print('El orden en Python es:',ordpy)
+
+
+horas = input('Ingrese horas trabajadas:')
+salario = input('Ingrese pago por hora:')
+h=int(horas)
+s=int(salario)
+pago = h * s
+print(pago)
+
+
+
+
+
+
+
+
+
